5aa25d961b4778f8cf62a7d387ab7fc58660d8d4
josteinklogetvedt <jostein.klogetvedt@gmail.com>
2021-04-07 12:32:55 +0200

    Finished Numerical Project

--- StatFys/Numerical_project/verlet(1).py
+++ StatFys/Numerical_project/verlet(1).py
@@ -1,33 +1,30 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-partNum=1.     # number of particles
+partNum=1     # number of particles
 dt=0.005       # time step 
 LX=10          # size of container 
 LY=10
 KK=100.0       # strength of repulsive wall force
-EPS=1          # strength of particle-particle interaction
+EPS=1.0          # strength of particle-particle interaction
 
 x = np.zeros(partNum)   # x cooordinate of particles
-y = np.zeros(partNum).  
+y = np.zeros(partNum) 
 vx = np.zeros(partNum)  # velocites 
 vy = np.zeros(partNum)
 ax = np.zeros(partNum)  #accelerations
 ay = np.zeros(partNum)
 
 
-
-x[0]=1
+x[0]=1     # initial position (with one particle)
 y[0]=2
 
-vx[0]=10
+
+vx[0]=10   #initial velocity
 vy[0]=-5
 
 
-
-
 for i in range (2000):
-    
     
      x[:] = x[:] + vx[:] * dt + (ax[:] * dt**2)/2        #first step of verlet integration
      y[:] = y[:] + vy[:] * dt + (ay[:] * dt**2)/2
@@ -39,8 +36,6 @@ for i in range (2000):
      ay[:] = 0
            
      for j in range (partNum):
-         
-      
          
         if (x[j] > LX):
           ax[j]+=-KK*(x[j]-LX)
@@ -59,10 +54,10 @@ for i in range (2000):
            dist13= (dist2**6)*dist
         
            dVdr=12*EPS*(1/dist13-1/dist7)
-           aa=(x[j] - x[k])*dVdr
+           aa=dVdr*(x[j] - x[k])/dist
            ax[j] += aa
            ax[k] += -aa
-           aa=(y[j] - y[k])*dVdr
+           aa=dVdr*(y[j] - y[k])/dist
            ay[j] += aa
            ay[k] += -aa
            
